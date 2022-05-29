cbf0838b444466cadbd31a9ce69081ba25ca25d2
Kemunto Gift <giftoyugi@gmail.com>
2022-05-29 17:18:27 +0300

    add new activity to display information

--- app/src/main/java/com/kemunto/ap1/TrackingActivity.java
+++ app/src/main/java/com/kemunto/ap1/TrackingActivity.java
@@ -2,13 +2,31 @@ package com.kemunto.ap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TrackingActivity extends AppCompatActivity {
+    private Button mSubmitButton;
+    private EditText mPersonNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
+        mPersonNameEditText = (EditText) findViewById(R.id.personNameEditText);
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mPersonNameEditText.getText().toString();
+                Intent intent = new Intent(TrackingActivity.this, InformationActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
 }
