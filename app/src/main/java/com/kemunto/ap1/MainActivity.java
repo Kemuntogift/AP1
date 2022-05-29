7093c300d9a61cc226f5a007154e6ba633ce072a
Kemunto Gift <giftoyugi@gmail.com>
2022-05-29 13:53:55 +0300

    add logic to redirect us to another activity when button is clicked

--- app/src/main/java/com/kemunto/ap1/MainActivity.java
+++ app/src/main/java/com/kemunto/ap1/MainActivity.java
@@ -2,13 +2,25 @@ package com.kemunto.ap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mGetTrackingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGetTrackingButton = (Button) findViewById(R.id.getTrackingButton);
+        mGetTrackingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TrackingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
