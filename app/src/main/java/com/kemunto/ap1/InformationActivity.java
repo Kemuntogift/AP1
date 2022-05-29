f7741f3bfc2252b6bf9ff6eb2aed375d082f6f1c
Kemunto Gift <giftoyugi@gmail.com>
2022-05-29 17:45:26 +0300

    add textView variables

--- app/src/main/java/com/kemunto/ap1/InformationActivity.java
+++ app/src/main/java/com/kemunto/ap1/InformationActivity.java
@@ -2,13 +2,20 @@ package com.kemunto.ap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InformationActivity extends AppCompatActivity {
-
+    private TextView mPersonNameTextView;
+    private TextView mYourLocationTextView;
+    private TextView mItemSizeTextView;
+    private TextView mWhereToTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
     }
 }
